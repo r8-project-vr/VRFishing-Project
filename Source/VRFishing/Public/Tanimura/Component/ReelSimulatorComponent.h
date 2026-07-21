8667f2cef7f9051d3065ca54c45a20a50714b43b
Tanimura <25ci0435@jec.ac.jp>
2026-07-21 18:59:14 +0900

    feature: マウスホイールを使ってリール巻き上げのデバッグができるようになった。

--- Source/VRFishing/Public/Tanimura/Component/ReelSimulatorComponent.h
+++ Source/VRFishing/Public/Tanimura/Component/ReelSimulatorComponent.h
@@ -10,7 +10,7 @@
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnRPMCalculated, float, NewRPM);
 
 /**
- * コントローラーのスティック回転や、キーボードでの入力から
+ * MetaQuestコントローラのスティック回転や、マウスホイールの回転から
  * 擬似的にエルゴメーターのRPMを算出するデバッグ用コンポーネント
  */
 
@@ -23,13 +23,17 @@ public:
 	// Sets default values for this component's properties
 	UReelSimulatorComponent();
 
-	// スティック入力と経過時間からRPM計算を更新
+	// スティック入力を基にRPMシミュレーションを実行
 	UFUNCTION(BlueprintCallable, Category = "Reel Simulator")
 	void SimulateReelByStick(FVector2D StickInput, float DeltaTime);
 
-	// キーボードから直接RPMの数値を入力する
+	// マウスホイールが1ノッチ回るたびに呼ばれ、RPMシミュレーションを実行
 	UFUNCTION(BlueprintCallable, Category = "Reel Simulator")
-	void SimulateReelByKey(float InputRPM);
+	void SimulateReelByWheel(float DeltaTime);
+
+	//// キーボードから直接RPMの数値を入力する
+	//UFUNCTION(BlueprintCallable, Category = "Reel Simulator")
+	//void SimulateReelByKey(float InputRPM);
 
 	// RPM算出時に実行されるイベント
 	UPROPERTY(BlueprintAssignable, Category = "Reel Simulator")
@@ -40,16 +44,17 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Reel Simulator|Config")
 	float StickThreshold;
 
+	// ホイール1ノッチ入力あたりの回転角度（ラジアン）
+	// デフォルトは15度 ≒ 0.2618rad
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Reel Simulator|Debug", meta = (ClampMin = "0.01"))
+	float WheelNotchAngleRad;
+
 private:
-	// 前フレームの入力角度（ラジアン）
-	float LastAngle;
+	// 角度変化量が1回転に達したらRPMを算出し、デリゲートを呼び出す
+	void CalculateRPM(float DeltaAngle, float DeltaTime);
 
-	// 累積角度（ラジアン）
-	float AccumulatedAngle;
-
-	// 現在の回転の経過時間（秒
-	float RevTime;
-
-	// 現在スティック入力を追跡中かどうか
-	bool bIsTracking;
+	float	LastAngle;				// 前フレームの入力角度（ラジアン）
+	float	AccumulatedAngleRad;	// 累積角度（ラジアン）
+	float	RevTime;				// 現在の回転の経過時間（秒）
+	bool	bIsTracking;			// 入力を追跡中かどうか
 };
