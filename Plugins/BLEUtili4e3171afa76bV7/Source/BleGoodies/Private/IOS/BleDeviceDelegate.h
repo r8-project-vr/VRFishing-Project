03a24f48da303716e533e84122eefb7c8bbb9218
KumoriLee <ear1223881481@gmail.com>
2026-07-22 11:22:15 +0900

    feature：BLE Goodiesを5.8に移植した

---
+++ Plugins/BLEUtili4e3171afa76bV7/Source/BleGoodies/Private/IOS/BleDeviceDelegate.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Nineva Studios
+
+#pragma once
+
+#include "Containers/UnrealString.h"
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface BleDeviceDelegate : NSObject<CBPeripheralDelegate>
+
+@property(nonatomic, copy) void (^onWrite)(FString ServiceUuid, FString CharacteristicUuid);
+@property(nonatomic, copy) void (^onRead)(FString ServiceUuid, FString CharacteristicUuid, NSData* Data);
+@property(nonatomic, copy) void (^onNotification)(FString ServiceUuid, FString CharacteristicUuid, NSData* Data);
+@property(nonatomic, copy) void (^onUnsubscribe)(FString ServiceUuid, FString CharacteristicUuid);
+@property(nonatomic, copy) void (^onError)(FString ServiceUuid, FString CharacteristicUuid, FString ErrorMessage);
+
+@end
