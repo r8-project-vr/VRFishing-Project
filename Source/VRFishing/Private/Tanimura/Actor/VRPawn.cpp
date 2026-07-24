dac155db05b9c3c414b0b563dae9f54ac8d1a590
Tanimura <25ci0435@jec.ac.jp>
2026-07-24 17:53:26 +0900

    feature: プレイヤーの右手に釣竿を付けた

--- Source/VRFishing/Private/Tanimura/Actor/VRPawn.cpp
+++ Source/VRFishing/Private/Tanimura/Actor/VRPawn.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Tanimura/Actor/VRPawn.h"
@@ -6,16 +6,9 @@
 #include "Tanimura/Component/FishingReelStateComponent.h"
 //#include "Tanimura/Component/CatchingSimulatorComponent.h"
 
-// === 追加：アタッチとスポーンに必要なヘッダーのインクルード ===
-#include "Engine/World.h"
-#include "Components/SkeletalMeshComponent.h"
-
 AVRPawn::AVRPawn()
 {
     PrimaryActorTick.bCanEverTick = false;
-
-    // === 追加：ソケット名の初期値設定 ===
-    RodSocketName = FName("Socket_Rod");
 
     // コンポーネントの生成
     HandHeightDetectorComponent = CreateDefaultSubobject<UHandHeightDetectorComponent>(TEXT("HandHeightDetectorComponent"));
@@ -28,14 +21,6 @@ AVRPawn::AVRPawn()
 void AVRPawn::BeginPlay()
 {
     Super::BeginPlay();
-
-    // === 追加：HandMeshComponent 未指定の場合、自身のアクターから自動取得 ===
-    if (!HandMeshComponent) {
-        HandMeshComponent = FindComponentByClass<USkeletalMeshComponent>();
-    }
-
-    // === 追加：ゲーム開始時に釣り竿を生成してアタッチ ===
-    SpawnAndAttachFishingRod();
 
     // 1. 誘うモード：魚がヒットしたらリールモードへ
     if (HandHeightDetectorComponent) {
@@ -54,40 +39,6 @@ void AVRPawn::BeginPlay()
 
     // 初期モード（誘うモード）に設定
     ChangeFishingMode(EFishingMode::Attract);
-}
-
-// === 追加：釣り竿の生成・アタッチ処理の実装 ===
-void AVRPawn::SpawnAndAttachFishingRod()
-{
-    UWorld* World = GetWorld();
-    if (!World || !FishingRodClass || !HandMeshComponent) {
-        return;
-    }
-
-    // 二重生成の防止
-    if (SpawnedFishingRod) {
-        SpawnedFishingRod->Destroy();
-    }
-
-    // 釣り竿アクターのスポーン設定
-    FActorSpawnParameters SpawnParams;
-    SpawnParams.Owner = this;
-    SpawnParams.Instigator = GetInstigator();
-
-    SpawnedFishingRod = World->SpawnActor<AActor>(FishingRodClass, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
-
-    if (SpawnedFishingRod) {
-        // ソケットにぴったり合わせるトランスフォームルール
-        FAttachmentTransformRules AttachmentRules(
-            EAttachmentRule::SnapToTarget,
-            EAttachmentRule::SnapToTarget,
-            EAttachmentRule::KeepWorld,
-            false
-        );
-
-        // 指定したソケットへアタッチ
-        SpawnedFishingRod->AttachToComponent(HandMeshComponent, AttachmentRules, RodSocketName);
-    }
 }
 
 void AVRPawn::ChangeFishingMode(EFishingMode NewMode)
