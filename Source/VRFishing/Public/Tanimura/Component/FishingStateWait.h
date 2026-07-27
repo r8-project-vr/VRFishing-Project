ee63202db102553ebd3346c9c62fe6e64bf5eedc
谷村竜一 <25ci0435@jec.ac.jp>
2026-07-27 09:13:45 +0900

    feature: メインゲーム画面で、釣りを開始する準備が完了してるかを確認するステートコンポーネントを作成

---
+++ Source/VRFishing/Public/Tanimura/Component/FishingStateWait.h
@@ -0,0 +1,49 @@
+// Copyright 2026 JEC ProjectVR TeamRehab. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tanimura/Component/FishingStateComponentBase.h"
+#include "FishingStateWait.generated.h"
+
+/**
+ * ゲーム開始直後の「待機モード」
+ * HUDを基準に、指定の高さ(初期値50cm)より低い位置で数秒(初期値2秒)待つと、次のモードへ
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class VRFISHING_API UFishingStateWait : public UFishingStateComponentBase
+{
+    GENERATED_BODY()
+
+public:
+    UFishingStateWait();
+
+	// 基底クラスのメンバ関数をオーバーライド
+    virtual void EnterState() override;                 // ステート開始時の処理
+    virtual void UpdateState(float DeltaTime) override; // ステート更新時の処理
+    virtual void ExitState() override;                  // ステート終了時の処理
+
+protected:
+    // 右手の現在位置を取得
+    UFUNCTION(BlueprintCallable, Category = "Fishing|Wait")
+    FVector GetRightHandLocation() const;
+
+    // HUD/カメラの現在位置を取得
+    UFUNCTION(BlueprintCallable, Category = "Fishing|Wait")
+    FVector GetHUDLocation() const;
+
+	// 準備完了（手を下ろしている）と判定する、HUDとの下向き距離 (cm)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fishing|Wait")
+    float RequiredDownDistance = 50.0f;
+
+	// 次のステートに進むために、準備完了状態でキープしなければいけない時間 (秒)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Fishing|Wait")
+    float RequiredWaitTime = 2.0f;
+
+private:
+    // 条件を満たしている累積時間
+    float CurrentWaitTime;
+
+    // 完了フラグ
+    bool bIsCompleted;
+};
